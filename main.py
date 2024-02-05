66b7b24dd10deb79abacb9bd64a84fe30eded572
Bittu Singh <56586371+Bittu2903@users.noreply.github.com>
2024-02-05 22:48:53 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+# func = lambda a, b, c : a + b
+# print(func(2,3))
+
+# a = [2,1,3,4,2]
+# a.sort()
+# print(sorted(a))
+# print(a)
+
+# import copy
+
+# a = 5
+# b = copy.copy(a)
+
+# print(a,b, id(a)==id(b))
+
+# c = copy.deepcopy(a)
+
+# print(a,c, id(a)==id(c))
+# print(id(c), id(a))
+
+
+# 1
+# 1 1 2
+# 1 1 1 2 3
+# 1 1 1 1 2 3 4
+
+# for i in range(1, 5):
+#     for j in range(i):
+#         print(1,end='')
+#     for k in range(2,i+1):
+#         print(k,end='')
+#     print('\n')
+
+# l1 = []
+# l1.append([])
+# print(l1)
+
